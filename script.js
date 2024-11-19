186044ccdf213fc1c42d088d5dd5cbc3a820db28
agnaka <agn.muleto@gmail.com>
2024-11-19 09:53:21 -0300

    Ajustes gerais

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const produtos = document.querySelectorAll('.produto');
-    const noProductsMessage = document.querySelector('.no-products');
-    if (produtos.length === 0) {
-        noProductsMessage.style.display = 'block';
-    }
-});
-                               
